03b383ca7a613b889db4ec4ca608248b31ab7250
tanuja1521 <44312128+tanuja1521@users.noreply.github.com>
2019-06-23 22:13:04 +0530

    Update digitrecognition using cnn.py

--- digitrecognition using cnn.py
+++ digitrecognition using cnn.py
@@ -10,7 +10,7 @@ from tensorflow import keras
  # Image dimension is 28 pixels by 28 pixels.
  img_rows, img_cols = 28, 28
  # Load dataset. Each pixel is 0-255 grayscale value. Train shape is (60000, 28, 28). Test shape is (10000, 28, 28)
- (x_train, y_train), (x_test, y_test) = mnist.load_data()
+ (x_train, y_train), (x_test, y_test) = kera.datasets.mnist.load_data()
  # Reshape dataset to (60000, 28, 28, 1) and (10000, 28, 28, 1).
  x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
  x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
@@ -25,16 +25,16 @@ from tensorflow import keras
  y_test = keras.utils.to_categorical(y_test, num_classes)
  # Define neural networks.
  model = Sequential()
- model.add(Conv2D(32, kernel_size=(3, 3),
+ model.add(keras.layers.Conv2D(32, kernel_size=(3, 3),
                   activation='relu',
                   input_shape=input_shape))
- model.add(Conv2D(64, (3, 3), activation='relu'))
- model.add(MaxPooling2D(pool_size=(2, 2)))
- model.add(Dropout(0.25))
- model.add(Flatten())
- model.add(Dense(128, activation='relu'))
- model.add(Dropout(0.5))
- model.add(Dense(num_classes, activation='softmax'))
+ model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+ model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+ model.add(keras.layers.Dropout(0.25))
+ model.add(keras.layers.Flatten())
+ model.add(keras.layers.Dense(128, activation='relu'))
+ model.add(keras.layers.Dropout(0.5))
+ model.add(keras.layers.Dense(num_classes, activation='softmax'))
  model.compile(loss=keras.losses.categorical_crossentropy,
                optimizer=keras.optimizers.Adadelta(),
                metrics=['accuracy'])
@@ -53,5 +53,5 @@ from tensorflow import keras
  correct_indices = np.nonzero(predictions == y_test)[0]
  incorrect_indices = np.nonzero(predictions != y_test)[0]
 
-print(len(correct_indices)," classified correctly")
-print(len(incorrect_indices)," classified incorrectly")
+ print(len(correct_indices)," classified correctly")
+ print(len(incorrect_indices)," classified incorrectly")
